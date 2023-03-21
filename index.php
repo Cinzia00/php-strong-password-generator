3edae97bfb59897add82b054f8b0070f0717b6de
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-03-21 16:20:42 +0100

    Feat creata funzione intval

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 $password_generator = 'ABCDEFGHIJklmnopqrstuvwxyz0123456789?!%$';
 $password_numero_caratteri = $_GET['password'];
 $password_generata = str_shuffle($password_generator);
-$nuova_password = substr($password_generata, 3);
-
+$nuova_password = substr($password_generata, 3, intval($password_numero_caratteri));
 
 var_dump($password_numero_caratteri);
 var_dump($password_generata);

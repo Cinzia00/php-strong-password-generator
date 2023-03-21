71cd16d17ff1b39dc4e73b3f1b47411ac525ca07
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-03-21 16:25:38 +0100

    Feat creato file functions

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-$password_generator = 'ABCDEFGHIJklmnopqrstuvwxyz0123456789?!%$';
-$password_numero_caratteri = $_GET['password'];
-$password_generata = str_shuffle($password_generator);
-$nuova_password = substr($password_generata, 3, intval($password_numero_caratteri));
-
-var_dump($password_numero_caratteri);
-var_dump($password_generata);
-var_dump($nuova_password );
+    include __DIR__ .'/functions.php'
 ?>
 <!DOCTYPE html>
 <html lang="en">
